97206704b7c16a5790de91911aeaf82b2e536221
sahalamol <148203587+sahala-mol@users.noreply.github.com>
2025-09-17 13:10:58 +0530

    Updated base.java

--- src/test/java/Base/base.java
+++ src/test/java/Base/base.java
@@ -4,9 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -15,24 +13,25 @@ import java.util.HashMap;
 
 public class base {
 
-	 public WebDriver driver;   // static
-	 public WebDriverWait wait;
-	    
-	@BeforeSuite
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @BeforeSuite
     public void initializeDriver() {
         WebDriverManager.chromedriver().setup();
+        
 
         ChromeOptions options = new ChromeOptions();
         options.setBinary("C:\\Users\\hp\\chromedriver\\chrome-win64\\chrome-win64\\chrome.exe");
-              
+
+        // ✅ No need for setBinary unless Chrome is not in default path
         HashMap<String, Object> prefs = new HashMap<>();
-        prefs.put("profile.password_manager_leak_detection", false); // Disables "Change Password" prompt/leak alerts
+        prefs.put("profile.password_manager_leak_detection", false);
         options.setExperimentalOption("prefs", prefs);
-        
+
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        
     }
 
     @AfterSuite
@@ -44,4 +43,3 @@ public class base {
         }
     }
 }
-	
