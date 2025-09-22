f250608136da0552a5d23d2ab67fe66a2d5c66fc
sahalamol <148203587+sahala-mol@users.noreply.github.com>
2025-09-22 21:15:50 +0530

    Update base.java

--- src/test/java/Base/base.java
+++ src/test/java/Base/base.java
@@ -4,8 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import java.time.Duration;
@@ -13,18 +13,16 @@ import java.util.HashMap;
 
 public class base {
 
-    public WebDriver driver;
-    public WebDriverWait wait;
+    public static WebDriver driver;
+    public static WebDriverWait wait;
 
-    @BeforeSuite
+    @BeforeTest
     public void initializeDriver() {
         WebDriverManager.chromedriver().setup();
-        
 
         ChromeOptions options = new ChromeOptions();
         options.setBinary("C:\\Users\\hp\\chromedriver\\chrome-win64\\chrome-win64\\chrome.exe");
 
-        // ✅ No need for setBinary unless Chrome is not in default path
         HashMap<String, Object> prefs = new HashMap<>();
         prefs.put("profile.password_manager_leak_detection", false);
         options.setExperimentalOption("prefs", prefs);
@@ -34,7 +32,7 @@ public class base {
         wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     }
 
-    @AfterSuite
+    @AfterTest
     public void tearDown() {
         System.out.println("Closing Browser...");
         if (driver != null) {
